62349e5cd0f1909e28e437899aa69a0b37ef506a
Devi Chandrika Vegi <dchandrikavegi@dcvegi-ltm.internal.salesforce.com>
2021-06-15 20:29:06 +0530

    third commit

--- src/server/api.js
+++ src/server/api.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 const HOST = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3002;
 
-const DIST_DIR = './src/client';
+const DIST_DIR = './dist';
 
 app.use(express.static(DIST_DIR));
 
