684687319502bfaa6df93f994ca13494655ef30a
Devi Chandrika Vegi <dchandrikavegi@dchandr-ltmvdad.internal.salesforce.com>
2022-05-26 16:40:56 +0530

    first commit

--- src/client/modules/my/process/process.js
+++ src/client/modules/my/process/process.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class Process extends LightningElement {}
+export default class Process extends LightningElement {
+    test() {
+        console.log('testing');
+    }
+
+}
---
+++ src/client/modules/th/allBadges/allBadges.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class allBadges extends LightningElement {}
